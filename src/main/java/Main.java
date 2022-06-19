207c5799342699a192eaa811cc7ebfda31aa3af2
key <rlfbd5142@gmail.com>
2022-06-19 18:57:06 +0900

    [feature] Thread 실행 코드 작성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import domain.Store;
+import thread.User;
+
+import java.util.ArrayList;
+
 public class Main {
+
 	public static void main(String[] args) {
 
+		int threadCount = 100;
+		int productCount = 10000;
+
+		// Store Open 준비
+		ArrayList<Long> products = new ArrayList<>(productCount);
+		for (int i = 0; i < productCount; i++) {
+			products.add((long) i);
+		}
+		Store store = new Store(products);
+
+		// 손님 입장 줄 선 긋는 중
+		Runnable[] customers = new Runnable[threadCount];
+
+		// 손님들을 입장 줄에 세워놓기
+		for (int i = 0; i < threadCount; i++) {
+			customers[i] = new User(store, "User-" + i);
+		}
+
+		// 손님들 입장
+		for (Runnable runnable : customers) {
+			Thread newThread = new Thread(runnable);
+			newThread.start();
+		}
 	}
 }
